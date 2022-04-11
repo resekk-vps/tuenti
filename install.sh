aa259c1c6df3832ee119cddbd192cdbdd5d7c947
RESEKK VPS <103382307+resekk-vps@users.noreply.github.com>
2022-04-11 09:33:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ cd /var/www; git clone https://github.com/resekk-vps/claro
 cd /var/www/tuenti; mv ccs /var/www; mv css /var/www; mv data /var/www; mv fonts /var/www; mv js /var/www; mv parts /var/www; mv config.php /var/www; mv favicon.png /var/www; mv index.php /var/www; mv send.php /var/www
 cd /var/www; nano config.php
 sudo service apache2 restart
+#BANNER
+clear && clear; cowsay -f eyes "SCRIPT FINALIZADA" | lolcat && figlet -f slant "RESEKK VPS" | lolcat
